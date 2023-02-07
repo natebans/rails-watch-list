260598e506f7c67cd0913ca0bd9420c7e47a20f1
Nate Banas <nate.banas@gmail.com>
2023-02-07 19:56:09 +0100

    coded routes, controllers, and views for lists and bookmarks

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :list_id, :movie_id)
+  end
+end
