ad229d78f5d89ee499bc419222b7c8be3f4e250e
Nate Banas <nate.banas@gmail.com>
2023-02-09 21:05:36 +0100

    updated to only allow one picture per list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,6 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    raise
     if @list.save
       redirect_to list_path(@list)
     else
@@ -29,6 +28,6 @@ class ListsController < ApplicationController
   end
 
   def list_params
-    params.require(:list).permit(:name, photos: [])
+    params.require(:list).permit(:name, :photo)
   end
 end
